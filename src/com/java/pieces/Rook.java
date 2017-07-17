b90d830b4e620e2e54f535eea7af0d9032e1bb8e
piyuri sahu <piyurisahu@gmail.com>
2017-07-17 10:25:41 +0530

    added TestRook and Rook

--- src/com/java/pieces/Rook.java
+++ src/com/java/pieces/Rook.java
@@ -11,6 +11,21 @@ public class Rook implements Piece{
 	
 	public ArrayList<Pair> getMovesFrom(int row, int col) {
 		ArrayList<Pair> listOfMoves=new ArrayList<Pair>();
+		listOfPositionInColumn(row, col, listOfMoves);
+		listOfPositionInRow(row, col, listOfMoves);
+		
+		return listOfMoves;
+	}
+
+	public  void listOfPositionInRow(int row, int col, ArrayList<Pair> listOfMoves) {
+		for(int i=0;i<8;i++)
+		{
+				if(i!=col)
+				listOfMoves.add(new Pair(row,i));
+		}
+	}
+
+	public void listOfPositionInColumn(int row, int col, ArrayList<Pair> listOfMoves) {
 		for(int i=0;i<8;i++)
 		{
 				if(i!=row)
@@ -18,13 +33,6 @@ public class Rook implements Piece{
 				listOfMoves.add(new Pair(i,col));
 				}
 		}
-		for(int i=0;i<8;i++)
-		{
-				if(i!=col)
-				listOfMoves.add(new Pair(row,i));
-		}
-		
-		return listOfMoves;
 	}
 	
 	public Set<Pair> getMovesFrom2(int row, int col) {
--- src/com/test/TestRook.java
+++ src/com/test/TestRook.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -19,9 +20,10 @@ public class TestRook {
 		Rook rook=new Rook();
 		ArrayList<Pair> actual=rook.getMovesFrom(3, 4);
 		assertFalse(actual.contains(new Pair(3,4)));
-		ArrayList<Pair> espected=rook.getMovesFrom(3, 4);
 		
-		
-		
+		ArrayList<Pair> expected=new ArrayList<Pair>();
+		rook.listOfPositionInColumn(3, 4, expected);
+		rook.listOfPositionInRow(3, 4, expected);
+		assertEquals(expected, actual);	
 	}
 }
